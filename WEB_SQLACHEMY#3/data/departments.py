49ad432efd3a76758500fdc57809b0d41383d217
Айдар Шайхутдинов <50844278+jezcall@users.noreply.github.com>
2020-02-28 19:30:12 +0300

    Форма регистрации
    Форма регистрации

---
+++ WEB_SQLACHEMY#3/data/departments.py
@@ -0,0 +1,17 @@
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class Departments(SqlAlchemyBase):
+    __tablename__ = 'departments'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
+    chief = sqlalchemy.Column(sqlalchemy.Integer)
+    members = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String)
+    user = orm.relation('User')
+    jobs = orm.relation('Jobs')
---
+++ WEB_SQLACHEMY#3/server.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template
+from data import db_session
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+
+from WEB_SQLACHEMY.data.db_session import global_init, create_session
+from WEB_SQLACHEMY.data.jobs import Jobs
+from WEB_SQLACHEMY.data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Login / email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Repeat Password', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    age = StringField('Age', validators=[DataRequired()])
+    position = StringField('Position', validators=[DataRequired()])
+    speciality = StringField('Speciality', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_session.global_init("db/mars_explorer.db")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            email=form.email.data,
+            hashed_password=form.password.data,
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.name.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
+
